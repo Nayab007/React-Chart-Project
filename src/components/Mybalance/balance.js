2e478a2b819c1f6508e526857f8ca3d6551aed30
Nayab Lone <nayab@c-management.se>
2023-12-09 01:14:24 +0100

    First commit

---
+++ src/components/Mybalance/balance.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './balance.css'
+import logo from '../../design/image/logo.svg';  
+
+
+function Balance() {
+  return (
+    <div className='head-container'>
+        <div className='mybalance'>My balance</div>
+    <div className='amount'>$ 921.48</div>
+    <img className="logo"
+    src={logo} alt="logo" />
+    </div>
+  )
+}
+
+export default Balance;

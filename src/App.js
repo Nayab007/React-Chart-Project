4421b792d13f58a184c7c334240a7980db09b439
Nayab Lone <nayab@c-management.se>
2023-12-10 02:08:32 +0100

    Chart compoent started

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, } from "react-router-dom";
 import Balance  from "./components/Mybalance/balance.js";
+import Chart from "./components/chart/chart.js"
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Balance />  
+        <Chart />
       </header>
     </div>
     </Router>
---
+++ src/components/chart/chart.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './chart.css';
+
+
+function Chart() {
+  return (
+    <div>chart</div>
+  )
+}
+
+export default Chart;

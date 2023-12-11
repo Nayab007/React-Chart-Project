9fc46640751a23fe868569d5c379d241d37e4b3d
Nayab Lone <nayab@c-management.se>
2023-12-11 07:36:12 +0100

    dollar sign added

--- src/components/chart/chart.js
+++ src/components/chart/chart.js
@@ -18,24 +18,42 @@ function Chart() {
     
     <div className="head-container">
       <Bar
-        data={{
-          labels: data.map((data) => data.day),
-          datasets: [
-            {
-              data: data.map((data) => data.amount),
-              backgroundColor: ["hsl(10, 79%, 65%)", "hsl(186, 34%, 60%)"],
-              borderRadius: 5,
-            },
-          ],
-        }}
-        options={{
-          plugins: {
-            title: {
-              text: "Spending - Last 7 Days",
-            },
+  data={{
+    labels: data.map((data) => data.day),
+    datasets: [
+      {
+        data: data.map((data) => data.amount),
+        backgroundColor: ["hsl(10, 79%, 65%)", "hsl(186, 34%, 60%)"],
+        borderRadius: 5,
+      },
+    ],
+  }}
+  options={{
+    plugins: {
+      title: {
+        text: "Spending - Last 7 Days",
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            let label = context.dataset.label || '';
+
+            if (label) {
+              label += ': ';
+            }
+            
+            if (context.parsed.y !== null) {
+              label += '$' + context.parsed.y.toFixed(2); // Add dollar sign and format amount
+            }
+
+            return label;
           },
-        }}
-      />
+        },
+      },
+    },
+  }}
+/>
+
       <div className="bottomText">
         Total this month
     </div>
